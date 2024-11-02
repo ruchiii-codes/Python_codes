11079525a8ada164d3d5fb6c58b2b177e4f751a0
ruchiii-codes <ruchi06052005@gmail.com>
2024-11-02 17:53:51 -0400

    Data Types

---
+++ DataType.py
@@ -0,0 +1,11 @@
+a = 1 # a is an integer 
+
+b = 5.33 # b is a floating point number
+
+c = "Ruchi" # c is a string
+
+d = False # d is a boolean variable
+
+e = None # e is a none type variable
+
+print(a,b,c,d,e)
