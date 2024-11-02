69547ffdd8fcae9bb06c3134721417bab3878852
ruchiii-codes <ruchi06052005@gmail.com>
2024-11-02 17:55:13 -0400

    Operators

---
+++ Operators.py
@@ -0,0 +1,36 @@
+# Arithmatic Operators
+a = 5
+b = 5
+c = a+b
+print(c)
+
+# Assignment Operators
+a = 4-2
+print(a)
+b = 6
+b += 3 # Increment the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+d = 5<4
+print(d)
+
+# Logical Operators
+e = True or False
+print(e)
+
+#Truth Table of 'OR'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+#Truth Table of 'AND'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+#Truth Table of 'NOT'
+print(not(False))  # F -> T
+print(not(True))   # T -> F
