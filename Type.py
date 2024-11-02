1855c0d8041be16b2a9d350b1582fdd6e0b571f7
ruchiii-codes <ruchi06052005@gmail.com>
2024-11-02 17:55:43 -0400

    Types

---
+++ Type.py
@@ -0,0 +1,14 @@
+# int Type
+a = 6
+t = type(a)   # <class 'int'>
+print(t)
+
+# floate Type
+b = 6.55
+t = type(b)   # <class 'floate'>
+print(t)
+
+# string Type
+c = "Ruchi"
+t = type(c)   # <class 'str'>
+print(t)
