0b4946335b9b3c02776fc1bb5addcaae7cec29a6
ruchiii-codes <ruchi06052005@gmail.com>
2024-11-11 22:27:46 -0500

    Negative Slicing

---
+++ Negative_slicing.py
@@ -0,0 +1,8 @@
+name= "Ruchi"     # Positive : R=0 u=1 c=2 h=3 i=4  Negative : R=-5 u=-4 c=-3 h=-2 i=-1
+print(name[0:3])
+
+print(name[-4:-1])   # Convert it into positive index
+print(name[1:4])     
+
+print(name[:4])   # Means [0:4]
+print(name[1:])   #Means [1:length of string] here [1:5]
