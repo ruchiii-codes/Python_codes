080eb84601b0d13aaa454476ea2553f91c4bc73a
ruchiii-codes <ruchi06052005@gmail.com>
2024-11-11 22:29:39 -0500

    Strings

---
+++ strings.py
@@ -0,0 +1,6 @@
+name = "Ruchi"      # R=0 u=1 c=2 h=3 i=4
+nameshort = name[0:3]  # Start from the index 0 all the way till 3 (excluding 3)
+print(nameshort)  
+
+character1 = name[1]
+print(character1)  # u 
